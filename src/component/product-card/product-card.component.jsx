0daffeee58e8f20315c68d9582f967b06577fd54
vanessaMarcuse <babacar.diakte01@gmail.com>
2022-05-04 16:44:35 -0400

    fixed some styliying

--- src/component/product-card/product-card.component.jsx
+++ src/component/product-card/product-card.component.jsx
@@ -14,9 +14,9 @@ const ProductCard = ({ product }) => {
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
         <span className="name">{name}</span>
-        <span className="price">{price}</span>
+        <span className="price">${price}</span>
       </div>
-      <Button buttonType="inverted" onClick={addProductToCart}>
+      <Button buttonType="google" onClick={addProductToCart}>
         Add to card
       </Button>
     </div>
